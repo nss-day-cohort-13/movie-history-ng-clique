8a47122d0d226c1a7223dbdb9347e4cc16846956
Castle Crawford <nathanscottcrawford@gmail.com>
2016-06-03 09:23:43 -0500

    able to search through OMDb API and print data to page based on movie title, added rating select and watched checkbox, firebase get request works.

---
+++ app/constants.js
@@ -0,0 +1,2 @@
+angular.module('app')
+  .constant('OMD_URL', 'http://www.omdbapi.com/')
---
+++ app/factories.js
@@ -0,0 +1,14 @@
+// angular.module('app')
+// .factory('movieFactory', ($http,API_URL) => {
+
+//   let movies = null
+
+//   return {
+//     getMovies() {
+//       return $http
+//         .get('http://www.omdbapi.com/?')
+//         .then(resolve => resolve.data)
+//         .then(m => movies = m)
+//     },
+//   }
+// })
